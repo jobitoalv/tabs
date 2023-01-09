86041605666f1223a399543ff23fc03a6262e236
Job Alvarez <jobitoalv@gmail.com>
2023-01-09 15:28:21 +1000

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
+import { GrGremlin } from "react-icons/gr";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  const [loading, setLoading] = useState(false)
-  
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  return <h2>tabs project setup<GrGremlin/></h2>
 }
 
 export default App
